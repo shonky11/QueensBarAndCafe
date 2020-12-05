4e31563cf3f3ff4398bfedb914474c868e94fc45
shonak <raspi3141@gmail.com>
2020-12-05 22:44:00 +0000

    home

--- app/src/main/java/com/qads/queensbarandcafe/fragments/ItemsFragment.java
+++ app/src/main/java/com/qads/queensbarandcafe/fragments/ItemsFragment.java
@@ -4,6 +4,6 @@ import androidx.fragment.app.Fragment;
 
 public class ItemsFragment extends Fragment {
 
-    
+
 
 }
--- app/src/main/java/com/qads/queensbarandcafe/helpers/MenuItem.java
+++ app/src/main/java/com/qads/queensbarandcafe/helpers/MenuItem.java
@@ -2,60 +2,45 @@ package com.qads.queensbarandcafe.helpers;
 
 public class MenuItem {
 
-    private String mProductName;
-    private String mProductDescription;
-    private Double mProductPrice;
-    private boolean mProductInStock;
-    private String mProductLocation;
+    private String mName;
+    private String mDescription;
+    private Double mPrice;
+    private boolean mStock;
+    private String mLocation;
     private String[] mOptions;
     private String[] mSize;
 
     public MenuItem(){
+
     }
 
-    public MenuItem(String productname, String productdescription, Double productprice, boolean productinstock,
-                    String productlocation, String[] options, String[] size){
-        mProductName = productname;
-        mProductDescription = productdescription;
-        mProductPrice = productprice;
-        mProductInStock = productinstock;
-        mProductLocation = productlocation;
+    public MenuItem(String productname, String productdescription, Double productprice, boolean productinstock, String productlocation, String[] options, String[] size){
+
+        mName = productname;
+        mDescription = productdescription;
+        mPrice = productprice;
+        mStock = productinstock;
+        mLocation = productlocation;
         mOptions = options;
         mSize = size;
 
     }
 
-    public String getmProductName(){return mProductName;}
-    public String getmProductDescription(){return mProductDescription;}
-    public String getmProductLocation(){return mProductLocation;}
-    public Double getmProductPrice(){return mProductPrice;}
-    public String[] getmOptions(){return mOptions;}
-    public String[] getmSize(){return mSize;}
+    public String getName(){return mName;}
+    public String getDescription(){return mDescription;}
+    public String getLocation(){return mLocation;}
+    public Double getPrice(){return mPrice;}
+    public String[] getOptions(){return mOptions;}
+    public String[] getSize(){return mSize;}
 
-
-    public void setmProductName(String productName) {
-        this.mProductName = productName;
-    }
-    public void setmProductDescription(String productDescription) {
-        this.mProductDescription = productDescription;
-    }
-    public void setmProductPrice(Double productPrice) {
-        this.mProductPrice = productPrice;
-    }
-    public void setmProductInStock(boolean productInStock) {
-        this.mProductInStock = productInStock;
-    }
-    public void setmProductLocation(String productLocation) {
-        this.mProductLocation = productLocation;
-    }
-    public void setmOptions(String[] options) {
+    public void setName(String productName) { this.mName = productName; }
+    public void setDescription(String productDescription) { this.mDescription = productDescription; }
+    public void setPrice(Double productPrice) { this.mPrice = productPrice; }
+    public void setInStock(boolean productInStock) { this.mStock = productInStock; }
+    public void setLocation(String productLocation) { this.mLocation = productLocation; }
+    public void setOptions(String[] options) {
         this.mOptions = options;
     }
-    public void setmSize(String[] size) {
-        this.mSize = size;
-    }
-
-
-
+    public void setSize(String[] size) { this.mSize = size; }
 
 }
