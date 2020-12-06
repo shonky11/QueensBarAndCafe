2d34c2eb80379756095ff1086b63673d54563171
George Mercer <gm620@cam.ac.uk>
2020-12-06 20:05:28 +0000

    Added MenuItem

--- app/src/main/java/com/qads/queensbarandcafe/fragments/ItemsFragment.java
+++ app/src/main/java/com/qads/queensbarandcafe/fragments/ItemsFragment.java
@@ -1,8 +1,42 @@
 package com.qads.queensbarandcafe.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.qads.queensbarandcafe.R;
 
 public class ItemsFragment extends Fragment {
+    private EditText editText, etd;
+    private Button button;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private FirebaseRecyclerAdapter adapter;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+
+
+
+
+    }
+
+
 
 
 
