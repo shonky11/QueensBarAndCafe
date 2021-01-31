fa0fa2530a177b756f395bf29142f843916b1a51
Rajan Mahoon <rm916@cam.ac.uk>
2021-01-31 23:30:48 +0000

    login activity added and still fixing the cart button

---
+++ app/src/main/java/com/qads/queensbarandcafe/activities/LoginActivity.java
@@ -0,0 +1,94 @@
+package com.qads.queensbarandcafe.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.qads.queensbarandcafe.R;
+import com.qads.queensbarandcafe.helpers.User;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText txtfirstname, txtlastname;
+    CardView signupbtn;
+
+    // Firebase instance variable
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private FirebaseDatabase mFirebaseDatabase;
+    //private DatabaseReference mFirebaseDatabaseReference;
+
+    //this will get the current instance of the document in cloud firestore
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    final String userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    //this retrieves the entire document with this specific uid - needed to update the profile information
+    private DocumentReference docRef = db.collection("users").document(userid);
+
+    private String TAG = "MyActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        txtfirstname = (EditText) findViewById(R.id.first);
+        txtlastname = (EditText) findViewById(R.id.last);
+
+        //gets current instance of the database
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        signupbtn = (CardView) findViewById(R.id.signupbtn);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mFirebaseAuth.getCurrentUser();
+        final String email = user.getEmail();
+        final String crsid = mFirebaseAuth.getCurrentUser().getEmail().split("@")[0];
+
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String first_name = txtfirstname.getText().toString().trim();
+                String last_name = txtlastname.getText().toString().trim();
+
+                if(first_name.isEmpty() || last_name.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Please fill out all the fields", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    User users = new User(crsid, mFirebaseAuth.getUid(), first_name, last_name, email);
+                    docRef.set(users)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "DocumentSnapshot written with ID: ");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error adding document", e);
+                                }
+                            });
+
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                }
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/qads/queensbarandcafe/helpers/User.java
+++ app/src/main/java/com/qads/queensbarandcafe/helpers/User.java
@@ -3,19 +3,49 @@ package com.qads.queensbarandcafe.helpers;
 public class User {
     private String mCrsid;
     private String mUid;
+    private String mFirstname;
+    private String mLastname;
+    private String mEmail;
 
     public User(){
 
     }
 
-    public User(String crsid, String uid){
+    public User(String crsid, String uid, String firstname, String lastname, String email){
         mCrsid = crsid;
         mUid = uid;
+        mFirstname = firstname;
+        mLastname = lastname;
+        mEmail = email;
 
     }
 
+    public String getFirstname() {
+        return mFirstname;
+    }
+
+    public void setFirstname(String mFirstname) {
+        this.mFirstname = mFirstname;
+    }
+
+    public String getLastname() {
+        return mLastname;
+    }
+
+    public void setLastname(String mLastname) {
+        this.mLastname = mLastname;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
     public String getCrsid(){return mCrsid;}
-    public String getmUid(){return mUid;}
+    public String getUid(){return mUid;}
 
     public void setCrsid(String crsid) {
         this.mCrsid = crsid;
